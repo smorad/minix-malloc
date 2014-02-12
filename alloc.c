ca91a81a2739ed40de6b11a0ce773ab3dc05c0fb
U-steven-desktop\Steven <steven@steven-desktop.(none)>
2014-02-11 18:59:24 -0800

    wut

--- alloc.c
+++ alloc.c
@@ -8,6 +8,7 @@ typedef struct {
 	unsigned count;
 	long beg;
 	long end;
+	unsigned page_size;
 
 } mem_ptr;
 
@@ -24,24 +25,25 @@ int buddy_init(long n_bytes, int parm1, mem_ptr mp){
 	if (power2(n_bytes)!=0)
 		return -1;
 	mp.beg = malloc(n_bytes);
-	mp.count++;
+	if (mp.beg == NULL)
+		return -1;
+	mp.page_size = pow(2, parm1);
 }
 
 int meminit(long n_bytes, unsigned int flags, int parm1, int* parm2){
 	int rv;
+	mp.count = 0;	
 	if((flags & 0x1)==0x1){
-		mp.count = 0;	
+		printf("0x1");
 		rv = buddy_init(n_bytes, parm1, mp);
 		
 	}
-	else if((flags & 0x4)==0x4){
-		if((flags & 0x08)==0x08){}
-		else if ((flags & 0x10)==0x10){}
-		else if ((flags & 0x20)==0x20){}
-		else if ((flags & 0x40)==0x40){}
+		else if((flags & 0x08)==0x08){printf("0x8\n");}
+		else if ((flags & 0x10)==0x10){printf("0x10\n");}
+		else if ((flags & 0x20)==0x20){printf("0x20\n");}
+		else if ((flags & 0x40)==0x40){printf("0x40\n");}
 		else{
-			printf("Invalid bits set: %b\n", flags);
+			printf("Invalid bits set: %#010x\n", flags);
 		}
-	}
 	return rv;
 }
