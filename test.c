9d59ebb8b5e713b592eda94b2583d2ce5c52c7bb
alynchos <alynchos@ucsc.edu>
2014-02-12 18:25:25 -0800

    Update test.c

--- test.c
+++ test.c
@@ -8,11 +8,7 @@ int main(){
 	printf("%d\n",meminit(128, 0x20, 0, 0));
 	printf("%d\n",meminit(128, 0x40, 0, 0));*/
 
-
-	meminit(128, 0x1, 0, 0);	
-	meminit(128, 0x4, 0, 0);
-	meminit(128, 0x8, 0, 0);
-	meminit(128, 0x10, 0, 0);
-	meminit(128, 0x20, 0, 0);
-	meminit(128, 0x40, 0, 0);
+	int handle;
+	handle = meminit(4096, 0x1, 4, 0);
+	memalloc(16,handle);
 }
