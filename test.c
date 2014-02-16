4a774f867fc501fb68d27f509dec35ddc4d4aec7
alynchos <alynchos@ucsc.edu>
2014-02-15 17:13:52 -0800

    Update test.c

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include "alloc.h"
+#include <stdio.h>
 
 int main(){
 	int handle;
@@ -7,12 +8,21 @@ int main(){
 	if(handle == -1){
 		printf("handle == -1\n");
 	}
-	memalloc(64,handle);
-	memalloc(16,handle);
-	memalloc(16,handle);
-	memalloc(64,handle);
-	void* mem_area = memalloc(2048,handle);
+	void* mem_area1 = memalloc(64,handle);
+	void* mem_area2 = memalloc(16,handle);
+	void* mem_area3 = memalloc(16,handle);
+	void* mem_area4 = memalloc(64,handle);
+	void* mem_area5 = memalloc(2048,handle);
 	// Test overflow
 	memalloc(2048,handle);
-	memfree(mem_area);
+	printf("mem_area1\n");
+	memfree(mem_area1);
+	printf("mem_area2\n");
+	memfree(mem_area2);
+	printf("mem_area3\n");
+	memfree(mem_area3);
+	printf("mem_area4\n");
+	memfree(mem_area4);
+	printf("mem_area5\n");
+	memfree(mem_area5);
 }
