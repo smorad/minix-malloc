e4597a6d5bfddcaa514c375de262f2b6e3ff4b9c
U-steven-desktop\Steven <steven@steven-desktop.(none)>
2014-02-20 17:54:30 -0800

    scripted commit

--- alloc.c
+++ alloc.c
@@ -109,7 +109,7 @@ int _buddy_init(long n_bytes, int parm1){
 btree find_by_region(btree root, void* region){	//will return node with segment, also will coalesce empty blocks
 	if(root==NULL) return NULL;
 	/*mark node as freeable, coalesce block*/
-	if(root->lchild==NULL && root->rchild==NULL && root->taken==0) {//leaf
+/*	if(root->lchild==NULL && root->rchild==NULL && root->taken==0) {//leaf
 		root->should_free = 1;
 	}
 	if(root->lchild!=NULL && root->rchild!=NULL)
@@ -117,7 +117,7 @@ btree find_by_region(btree root, void* region){	//will return node with segment,
 			free(root->lchild);
 			free(root->rchild);
 			root->lchild = root->rchild = NULL;
-		}
+		}*/
 	/*search block*/
 
 	//printf("root->seg_start: %p region: %p\n", root->seg_start, region);
