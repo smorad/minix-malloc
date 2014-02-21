7dce7e98c3c8f5c83cf38fd614c5d3b5a2ad27dc
U-steven-desktop\Steven <steven@steven-desktop.(none)>
2014-02-20 19:48:19 -0800

    scripted commit

--- test.c
+++ test.c
@@ -122,14 +122,14 @@ void buddy_holes(int page_size){
 	big *b;
 	int i;
 	for(i=0; i<64; i++)
-		b = memalloc(128, h);	
+		b = memalloc(64, h);	
 	count_holes(h, 0x1);
 }
 
 int main(){
 	char* test = malloc(1024);
 	test_buddy();
-	buddy_holes(12);
+	buddy_holes(6);
 	//aux_test_buddy();
 	//speed_test_buddy();
 	/*speed_test_list(0x4 | 0x0, 4); //first fit
