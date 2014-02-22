3f733c745ac017ed4cc1d47a35d34fdb22813f08
alynchos <alynchos@ucsc.edu>
2014-02-21 17:24:17 -0800

    Update test.c

--- test.c
+++ test.c
@@ -22,25 +22,20 @@ void test_list(){
 	if(handle == -1){
 		printf("handle == -1\n");
 	}
-	/*
+	
 	void* mem_area1 = memalloc(64,handle);
 	void* mem_area2 = memalloc(16,handle);
 	void* mem_area3 = memalloc(16,handle);
 	void* mem_area4 = memalloc(64,handle);
 	void* mem_area5 = memalloc(2048,handle);
-	// Test overflow
-	memalloc(2048,handle);
+	void* mem_area6 = memalloc(1024,handle);
 	memfree(mem_area1);
 	memfree(mem_area2);
 	memfree(mem_area3);
 	memfree(mem_area4);
 	memfree(mem_area5);
 	mem_area1 = memalloc(64,handle);
-	*/
-	int* test_array = memalloc(sizeof(int),handle);
-	test_array[0] = 32;
-	printf("Test_array[0]: %d\n", test_array[0]);
-	memfree(test_array);
+
 	count_holes(handle);
 }
 
