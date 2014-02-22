4b358f66e90a044f692da5455791ac3e79ffb945
alynchos <alynchos@ucsc.edu>
2014-02-21 18:54:19 -0800

    Update alloc.c

--- alloc.c
+++ alloc.c
@@ -685,7 +685,6 @@ void count_holes_list(metrics *m){
 	printf("\n------------------------------------------------\n\n");
 	printf("\n\n---------------------DATA----------------------\n\n");
 	qsort(free_holes, sizeof(unsigned long), mp.bitmap_size, comp);
-	int i = 0;
 	for(i = 0; i < mp.bitmap_size; i++) printf("%d\n",free_holes[i]);
 	qsort(taken_holes, sizeof(unsigned long), mp.bitmap_size, comp);
 	print_min(free_holes, taken_holes);
