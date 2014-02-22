e42803d47d73ae5677d638b53652f2dfde0cf555
alynchos <alynchos@ucsc.edu>
2014-02-21 20:04:25 -0800

    Update alloc.c

--- alloc.c
+++ alloc.c
@@ -697,14 +697,10 @@ void count_holes_list(metrics *m){
 	// Conduct experiments with our arrays
 	printf("\n------------------------------------------------\n\n");
 	printf("\n\n---------------------DATA----------------------\n\n");
-	//for(i = 0; i < m->num_free; i++) printf("%d\n",free_holes[i]);
-	unsigned long test[5] = {3,4,1,2,6};
-	for(i = 0; i < 5; i++) printf("%lu\n",test[i]);
+	for(i = 0; i < mp.bitmap_size; i++) printf("%d\n",free_holes[i]);
 	printf("\n------------------------------------------------\n\n");
-	qsort(test, 5, sizeof(unsigned long), comp);
-	for(i = 0; i < 5; i++) printf("%lu\n",test[i]);
-	//qsort(free_holes, (size_t) m->num_free, sizeof(free_holes[0]), (int (*) (const void*, const void*)) comp);
-	//for(i = 0; i < m->num_free; i++) printf("%d\n",free_holes[i]);
+	qsort(free_holes, mp.bitmap_size, sizeof(free_holes[0]), (int (*) (const void*, const void*)) comp);
+	for(i = 0; i < mp.bitmap_size; i++) printf("%d\n",free_holes[i]);
 	//qsort(taken_holes, m->num_taken, sizeof(unsigned long), comp);
 	print_min(free_holes, taken_holes);
 	print_max(free_holes, taken_holes, m);
